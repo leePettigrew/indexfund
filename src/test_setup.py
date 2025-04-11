ec39103f101b2de24ee42168f763131cf36110e1
leePettigrew <150901334+leePettigrew@users.noreply.github.com>
2025-04-11 20:35:08 +0100

    init and test

---
+++ src/test_setup.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+print("Numpy version:", np.__version__)
+print("Pandas version:", pd.__version__)
+print("yfinance imported successfully!")
+plt.plot([0, 1, 2], [0, 1, 4])
+plt.title("Test Plot")
+plt.show()
